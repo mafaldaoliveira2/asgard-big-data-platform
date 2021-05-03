c060d7b6c0ff223683e96c938404aae7bd833c82
Hugo Carvalho <hugodanielsilvacarvalho.hc@gmail.com>
2021-05-03 12:18:27 +0100

    Presto customize

--- prestodb/dev/build-presto-image.sh
+++
@@ -1,1 +0,0 @@
-sudo docker build --build-arg PRESTO_VERSION=0.251 -t asgard/prestodb:latest ./prestodb/
--- prestodb/dev/launch-presto.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run -it -d -p 8081:8080 -p 8182:8181 --network hadoop --name presto asgard/prestodb:latest
